31749585ddf49b99eadc87b0d23161d8551355ae
tomcat <tomcat.cheng@microlife.com.tw>
2019-05-28 17:12:17 +0800

    add fit API code. NOT yet !!

--- app/src/main/java/com/soft/fitdemo/MainActivity.java
+++ app/src/main/java/com/soft/fitdemo/MainActivity.java
@@ -1,16 +1,29 @@
 package com.soft.fitdemo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.soft.fitdemo.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 {
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 10;
+
     private TextView    tvMsg;
+
+    FitnessOptions fitnessOptions;
 
 
 
@@ -26,6 +39,16 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+                accessGoogleFit();
+            }
+        }
     }
 
     @Override
@@ -49,10 +72,38 @@ public class MainActivity extends AppCompatActivity
     private void initControl()
     {
         tvMsg.append("\n\t Start App.");
+
+        fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+        DataSource.Builder bloodPressureSource = (DataSource.Builder) new DataSource.Builder().setDataType();
     }
 
 
+    private void permissionCheck()
+    {
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            accessGoogleFit();
+        }
+    }
+
+    private void accessGoogleFit() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.YEAR, -1);
+        long startTime = cal.getTimeInMillis();
 
 
-}
+
+
+    }
 
