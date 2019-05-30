5f61cbd0864b20fce5916517930143e2924a2359
tomcat <tomcat.cheng@microlife.com.tw>
2019-05-30 14:08:14 +0800

    debug

--- app/src/main/java/com/soft/fitdemo/MainActivity.java
+++ app/src/main/java/com/soft/fitdemo/MainActivity.java
@@ -6,17 +6,28 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.HealthDataTypes;
+import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -25,10 +36,12 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 10;
+    private static final int REQUEST_LOCATION = 1;
 
     private TextView    tvMsg;
 
     FitnessOptions fitnessOptions;
+    private static OnDataPointListener mGoogleApiClient;
 
 
 
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG, "onCreate(), ");
 
         initView();
         initControl();
@@ -44,11 +58,17 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
+        Log.w(TAG, "onStart(), ");
     }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        Log.w(TAG, "onActivityResult(), requestCode: " + requestCode +
+                        ", resultCode: " + resultCode +
+                        "\n ,data: " + data);
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
                 accessGoogleFit();
@@ -62,25 +82,42 @@ public class MainActivity extends AppCompatActivity
                                            @NonNull int[] grantResults) {
 
         //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCATION) {
-            if(grantResults.length == 1
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // We can now safely use the API we requested access to
-                Location myLocation =
-                        LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            } else {
-                // Permission was denied or request was cancelled
-            }
+        Log.w(TAG, "onRequestPermissionsResult(), requestCode: " + requestCode +
+                        ", permissions: " + Arrays.toString(permissions) +
+                        ", grantResults: " + Arrays.toString(grantResults));
+        //if (requestCode == REQUEST_LOCATION) {
+        //    if(grantResults.length == 1
+        //            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        //        // We can now safely use the API we requested access to
+        //        //Location myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        //    } else {
+        //        // Permission was denied or request was cancelled
+        //    }
+        //}
+
+        switch (requestCode) {
+            case REQUEST_LOCATION:
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {}
+                break;
+
+            case GOOGLE_FIT_PERMISSIONS_REQUEST_CODE:
+                //initialize(this);
+                //permissionCheck();
+                break;
         }
+
     }
 
     @Override
     protected void onStop() {
+        Log.w(TAG, "onStop(), ");
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+        Log.w(TAG, "onDestroy(), ");
         super.onDestroy();
     }
 
@@ -89,25 +126,31 @@ public class MainActivity extends AppCompatActivity
     //------------------- User define function -----------------------//
     private void initView()
     {
+        Log.w(TAG, " initView(), ");
         tvMsg = findViewById(R.id.tv_MSG);
     }
 
     private void initControl()
     {
+        Log.w(TAG, " initControl(), ");
         tvMsg.append("\n\t Start App.");
 
-        fitnessOptions = FitnessOptions.builder()
-                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                .build();
+        //permissionCheck();
+        initialize(this);
 
-        DataSource.Builder bloodPressureSource = (DataSource.Builder) new DataSource.Builder()
-                .setDataType(HealthDataTypes.TYPE_BLOOD_PRESSURE);
+        //fitnessOptions = FitnessOptions.builder()
+        //        .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+        //        .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+        //        .build();
+        //
+        //DataSource.Builder bloodPressureSource = (DataSource.Builder) new DataSource.Builder()
+        //        .setDataType(HealthDataTypes.TYPE_BLOOD_PRESSURE);
     }
 
 
     private void permissionCheck()
     {
+        Log.w(TAG, " permissionCheck(), ");
         if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
             GoogleSignIn.requestPermissions(
                     this, // your activity
@@ -115,11 +158,13 @@ public class MainActivity extends AppCompatActivity
                     GoogleSignIn.getLastSignedInAccount(this),
                     fitnessOptions);
         } else {
+            initialize(this);
             accessGoogleFit();
         }
     }
 
     private void accessGoogleFit() {
+        Log.w(TAG, " accessGoogleFit(), ");
         Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
         long endTime = cal.getTimeInMillis();
@@ -127,6 +172,54 @@ public class MainActivity extends AppCompatActivity
         long startTime = cal.getTimeInMillis();
 
 
+    }
+
+
+    public static void initialize(final FragmentActivity activity){
+        Log.w(TAG, " initialize(), ");
+        // Setup Callback listener
+        GoogleApiClient.ConnectionCallbacks connectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(Bundle bundle) {
+                Log.i(TAG, "Connected! ");
+                // Now you can make calls to the Fitness APIs.
+                //subscribe();
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+                // If your connection to the sensor gets lost at some point,
+                // you'll be able to determine the reason and react to it here.
+                if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                    Log.i(TAG, "1 Connection lost.  Cause: Network Lost.");
+                } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                    Log.i(TAG, "2 Connection lost.  Reason: Service Disconnected");
+                }
+            }
+        };
+
+        // Handle Failed connection
+        GoogleApiClient.OnConnectionFailedListener connectionFailed = new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult result) {
+
+                Log.i(TAG, "3 Google Play services connection failed. Cause: " + result.toString());
+
+                Toast.makeText(activity, "4 Exception while connecting to Google Play services: " +
+                        result.getErrorMessage() + ":" + result.getErrorCode(), Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
+
+        // Create Google Api Client
+        mGoogleApiClient = (OnDataPointListener) new GoogleApiClient.Builder(activity)
+                .addConnectionCallbacks(connectionCallbacks)
+                .enableAutoManage(activity, connectionFailed)
+                .addScope(new Scope(Scopes.FITNESS_BODY_READ_WRITE))
+                .addApi(Fitness.HISTORY_API)
+                .build();
+        //mGoogleApiClient.connect();
     }
 }
 
